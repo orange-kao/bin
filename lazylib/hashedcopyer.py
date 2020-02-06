a18de330f783f64fb3df319e56bdd510730d8c38
Orange Kao <orange@kaosy.org>
2020-02-07 07:50:49 +1100

    digest-copy: support multiple source dir
    Support multiple source dir
    Support more filter
    Code cleanup

--- lazylib/hashedcopyer.py
+++ lazylib/hashedcopyer.py
@@ -3,28 +3,32 @@
 import os
 import hashlib
 import shutil
+import socket
+import re
+import sys
 
 class HashedCopyer:
     def __init__(self, dst_dir_list):
         self.dst_dir_list = dst_dir_list
 
     @staticmethod
-    def generate_file_sha512(file_name):
+    def is_valid_sha512(sha512_hex):
+        pattern = re.compile("\A[0-9a-f]{128}\Z")
+        if pattern.match(sha512_hex):
+            return True
+        return False
+
+    @staticmethod
+    def generate_file_sha512(file_pathname):
         sha512 = hashlib.sha512()
-        with open(file_name, 'rb') as f:
+        with open(file_pathname, 'rb') as f:
             for byte_block in iter(lambda: f.read(4096),b''):
                 sha512.update(byte_block)
         return sha512.hexdigest()
 
-    def is_exist_at_any_dst(self, sha512):
+    def find_exist_dst(self, dst_basename):
         for dir_cand in self.dst_dir_list:
-            if os.path.isfile(os.path.join(dir_cand, sha512)) is True:
-                return True
-        return False
-
-    def find_exist_dst(self, sha512):
-        for dir_cand in self.dst_dir_list:
-            if os.path.isfile(os.path.join(dir_cand, sha512)) is True:
+            if os.path.isfile(os.path.join(dir_cand, dst_basename)) is True:
                 return dir_cand
         return None
 
@@ -41,50 +45,50 @@ class HashedCopyer:
 
         return dst_dir
 
-    def copy(self, src_file):
-        print(f"{repr(src_file)}")
-        print(f"    -> ", end="", flush=True)
-        sha512 = self.generate_file_sha512(src_file)
-        print(f"{repr(sha512)}")
-
-        if self.is_exist_at_any_dst(sha512) is True:
-            print("    Already exist, do nothing.")
-            return
-
-        dst_dir = self.get_writable_dst_dir()
-        dst_file = os.path.join(dst_dir, sha512)
-        dst_temp_file = os.path.join(dst_dir, "temp-file")
-
-        try:
-            shutil.copyfile(src_file, dst_temp_file)
-            os.rename(dst_temp_file, dst_file)
-            print("    Not exist, copied.")
-        except KeyboardInterrupt as e:
-            print("Clearning up partial copy...")
-            os.remove(dst_temp_file)
-            raise e
-
-    def move_src_repo(self, src_file):
-        print(f"{repr(src_file)}")
-        file_basename = os.path.basename(src_file)
-
-        dst_dir = self.find_exist_dst(file_basename)
+    def __copy(self, src_pathname, dst_basename, *, del_src=False):
+        src_basename = os.path.basename(src_pathname)
+        dst_dir = self.find_exist_dst(dst_basename)
         if dst_dir is not None:
-            os.remove(src_file)
-            print(f"    Already exist in {dst_dir}, source deleted.")
+            print(f"    Already exist in {dst_dir}")
+            if del_src:
+                os.remove(src_pathname)
+                print(f"    Source deleted")
             return
 
         dst_dir = self.get_writable_dst_dir()
-        dst_file = os.path.join(dst_dir, file_basename)
-        dst_temp_file = os.path.join(dst_dir, "temp-file")
+        dst_pathname = os.path.join(dst_dir, dst_basename)
+        dst_temp_basename = "temp_{}_{}".format(socket.gethostname(), os.getpid())
+        dst_temp_pathname = os.path.join(dst_dir, dst_temp_basename)
 
         try:
-            shutil.copyfile(src_file, dst_temp_file)
-            os.rename(dst_temp_file, dst_file)
-            os.remove(src_file)
-            print(f"    Moved to {dst_dir}")
+            shutil.copyfile(src_pathname, dst_temp_pathname)
+            os.rename(dst_temp_pathname, dst_pathname)
+            if del_src:
+                os.remove(src_pathname)
+                print(f"    Moved to {dst_dir}")
+            else:
+                print(f"    Copied to {dst_dir}")
         except KeyboardInterrupt as e:
             print("Clearning up partial copy...")
-            os.remove(dst_temp_file)
+            if os.path.isfile(dst_temp_pathname):
+                os.remove(dst_temp_pathname)
             raise e
+
+    def copy(self, src_pathname):
+        print(repr(src_pathname))
+        print(f"    -> ", end="", flush=True)
+        sha512 = self.generate_file_sha512(src_pathname)
+        print(repr(sha512))
+
+        self.__copy(src_pathname, sha512, del_src=False)
+
+    def move_src_repo(self, src_pathname):
+        print(repr(src_pathname))
+        src_basename = os.path.basename(src_pathname)
+
+        if self.is_valid_sha512(src_basename) is not True:
+            print("    ERROR: File name is not SHA512")
+            sys.exit(1)
+
+        self.__copy(src_pathname, src_basename, del_src=True)
 
